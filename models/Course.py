9083b6a107a1df75e1b1be9fa41e5686361ff39a
Víctor Arreaga <arreagav17@gmail.com>
2022-09-05 10:54:05 -0500

    Refactor

--- models/Course.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-from models.Student import Student
-
-class Course(BaseModel):
-    name: str
-    students: List[Student]
--- services/StudentService.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import declarative_base
-
-class StudentService:
-  def __init__(self):
-    self.__DATABASE_URL = 'sqlite:///database.db'
-    
-
-
