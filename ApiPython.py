a32a95e7b090ed36677b4a5952ff5773a9b15f1e
Víctor Arreaga <arreagav17@gmail.com>
2022-09-05 10:57:08 -0500

    Refactor

--- ApiPython.py
+++ ApiPython.py
@@ -1,28 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-
-# from fastapi.middleware.cors import CORSMiddleware
 from starlette.exceptions import HTTPException as StarletteHTTPException
-
 from controllers.StudentController import StudentController
 
-# from routers import developers, users
-
 app = FastAPI()
-# origins = [
-#     '*'
-#     "http://localhost:3000",
-#     "https://apivelopers.com",
-#     "https://www.apivelopers.com",
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 
 @app.exception_handler(StarletteHTTPException)
@@ -30,10 +11,9 @@ async def http_exception_handler(request, exc):
     return JSONResponse(status_code=500, content={"Message": "Ha ocurrido un error"})
 
 
-@app.get("/api")
+@app.get("/")
 def home():
-    return JSONResponse(status_code=200, content={"Hello": "World"})
+    return JSONResponse(status_code=200, content={"message": "API Working!!"})
 
 
 app.include_router(StudentController().router)
-# app.include_router(users.router)
