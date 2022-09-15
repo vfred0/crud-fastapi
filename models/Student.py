806e460586f240676d500c6c8e3157552a82f924
Víctor Arreaga <arreagav17@gmail.com>
2022-09-15 14:54:25 -0500

    Model and table for database

--- models/Student.py
+++ models/Student.py
@@ -1,9 +1,16 @@
-import uuid
-from pydantic import BaseModel
 from typing import List, Optional
+import uuid
+
+# from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
+
+from utils.Skill import Skill
 
 
-class Student(BaseModel):
-    id: Optional[int]
-    name: str
-    age: int
+class Student(SQLModel, table=True):
+    id: Optional[uuid.UUID] = Field(default=uuid.uuid4(), primary_key=True)
+    name: str = Field(max_length=25, nullable=False)
+    age: int = Field(default=0, ge=0, le=100, nullable=False)
+    #skills: List[Skill] = Relationship(back_populates="id")
+
+
